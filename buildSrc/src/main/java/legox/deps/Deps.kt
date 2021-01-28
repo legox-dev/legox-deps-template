ef3a50f06aeeef276c145c083d92b4e26d3d7811
Trevor Wang <trevor.wang@qq.com>
2021-01-28 10:44:41 +0800

    build src

--- buildSrc/src/main/java/legox/deps/Deps.kt
+++ buildSrc/src/main/java/legox/deps/Deps.kt
@@ -1,7 +1,7 @@
 package legox.deps
 
 object Versions {
-    const val retrofit = "2.9.0"
+    const val retrofit = "2.4.0"
     const val appcompat = "1.2.0"
     const val coreKtx = "1.3.2"
     const val constraintLayout = "2.0.4"
